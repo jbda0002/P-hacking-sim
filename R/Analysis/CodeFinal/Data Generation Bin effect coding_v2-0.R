99c8c997c5e1a34d81e415bc00f3693db612825b
jbda0002 <44018316+jbda0002@users.noreply.github.com>
2020-02-10 11:37:54 +0100

    Effect coding

---
+++ R/Analysis/CodeFinal/Data Generation Bin effect coding_v2-0.R
@@ -0,0 +1,88 @@
+### Data Generation Bin ###
+library(BinNor)
+dataGen1 <- function(N,cor){
+  
+  no.bin=2; no.nor=1
+  mean.vec.nor=c(0); var.nor=c(1)
+  prop.vec.bin=c(0.5,0.5)
+  
+  M = matrix(c(1, 0,0,
+               0, 1,cor,
+               0,cor,1
+               
+  ), nrow=3, ncol=3)
+  sigma.star=compute.sigma.star( no.nor=1,no.bin=2, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  mydata[ mydata == 0 ] <- -1
+  names(mydata)<-c("h1","x1","y1")
+  
+  
+  return(mydata)
+}
+dataGen2 <- function(N,cor){
+  
+  no.bin=3; no.nor=1
+  mean.vec.nor=c(0); var.nor=c(1)
+  prop.vec.bin=c(0.5,0.5,0.5)
+  
+  M = matrix(c(1, 0,0,0,
+               0, 1,0,cor,
+               0,0,1,cor,
+               0,cor,cor,1
+               
+  ), nrow=4, ncol=4)
+  sigma.star=compute.sigma.star( no.nor=1,no.bin=3, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("h1","x1","x2","y1")
+  mydata[ mydata == 0 ] <- -1
+  
+  return(mydata)
+}
+dataGen3 <- function(N,cor){
+  no.bin=4; no.nor=1
+  mean.vec.nor=c(0); var.nor=c(1)
+  prop.vec.bin=c(0.5,0.5,0.5,0.5)
+  
+  M = matrix(c(1, 0,0,0,0,
+               0, 1,0,0,cor,
+               0,0,1,0,cor,
+               0,0,0,1,cor,
+               0,cor,cor,cor,1
+               
+  ), nrow=5, ncol=5)
+  sigma.star=compute.sigma.star( no.nor=1,no.bin=4, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("h1","x1","x2","x3","y1")
+  mydata[ mydata == 0 ] <- -1
+  return(mydata)
+}
+dataGen4 <- function(N,cor){
+  no.bin=5; no.nor=1
+  mean.vec.nor=c(0); var.nor=c(1)
+  prop.vec.bin=c(0.5,0.5,0.5,0.5,0.5)
+  
+  M = matrix(c(1, 0,0,0,0,0,
+               0, 1,0,0,0,cor,
+               0,0,1,0,0,cor,
+               0,0,0,1,0,cor,
+               0,0,0,0,1,cor,
+               0,cor,cor,cor,cor,1
+               
+  ), nrow=6, ncol=6)
+  sigma.star=compute.sigma.star( no.nor=1,no.bin=5, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("h1","x1","x2","x3","x4","y1")
+  mydata[ mydata == 0 ] <- -1
+  return(mydata)
+}
---
+++ R/Analysis/CodeFinal/Data Generation BinNormal effect coding_v2-0.R
@@ -0,0 +1,91 @@
+### Data Generation Bin H_1 is Normal ###
+
+library(BinNor)
+dataGen1 <- function(N,cor){
+  
+  no.bin=1; no.nor=2
+  mean.vec.nor=c(0,0); var.nor=c(1,1)
+  prop.vec.bin=c(0.5)
+  
+  M = matrix(c(1, 0,cor,
+               0, 1,0,
+               cor,0,1
+               
+  ), nrow=3, ncol=3)
+  sigma.star=compute.sigma.star( no.nor=2,no.bin=1, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("x1","h1","y1")
+  mydata[ mydata == 0 ] <- -1
+  
+  
+  return(mydata)
+}
+dataGen2 <- function(N,cor){
+  
+  no.bin=2; no.nor=2
+  mean.vec.nor=c(0,0); var.nor=c(1,1)
+  prop.vec.bin=c(0.5,0.5)
+  
+  M = matrix(c(1, 0,0,cor,
+               0, 1,0,cor,
+               0,0,1,0,
+               cor,cor,0,1
+               
+  ), nrow=4, ncol=4)
+  sigma.star=compute.sigma.star( no.nor=2,no.bin=2, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("x1","x2","h1","y1")
+  mydata[ mydata == 0 ] <- -1
+  
+  
+  return(mydata)
+}
+dataGen3 <- function(N,cor){
+  no.bin=3; no.nor=2
+  mean.vec.nor=c(0,0); var.nor=c(1,1)
+  prop.vec.bin=c(0.5,0.5,0.5)
+  
+  M = matrix(c(1, 0,0,0,cor,
+               0, 1,0,0,cor,
+               0,0,1,0,cor,
+               0,0,0,1,0,
+               cor,cor,cor,0,1
+               
+  ), nrow=5, ncol=5)
+  sigma.star=compute.sigma.star( no.nor=2,no.bin=3, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("x1","x2","x3","h1","y1")
+  mydata[ mydata == 0 ] <- -1
+  return(mydata)
+}
+dataGen4 <- function(N,cor){
+  no.bin=4; no.nor=2
+  mean.vec.nor=c(0,0); var.nor=c(1,1)
+  prop.vec.bin=c(0.5,0.5,0.5,0.5)
+  
+  M = matrix(c(1, 0,0,0,0,cor,
+               0, 1,0,0,0,cor,
+               0,0,1,0,0,cor,
+               0,0,0,1,0,cor,
+               0,0,0,0,1,0,
+               cor,cor,cor,cor,0,1
+               
+  ), nrow=6, ncol=6)
+  sigma.star=compute.sigma.star( no.nor=2,no.bin=4, prop.vec.bin=prop.vec.bin,
+                                 corr.mat=M)
+  mydata=as.data.frame(jointly.generate.binary.normal(N,no.bin,no.nor,prop.vec.bin,
+                                                      mean.vec.nor,var.nor, sigma_star=sigma.star$sigma_star))
+  
+  names(mydata)<-c("x1","x2","x3","x4","h1","y1")
+  mydata[ mydata == 0 ] <- -1
+  return(mydata)
+}
