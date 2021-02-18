5fdd32149a20427c14d5ce476d3fb395fc40a251
jbda0002 <44018316+jbda0002@users.noreply.github.com>
2021-02-18 10:56:55 +0100

    remove dub

--- manuscript/R/Analysis/CodeFinal/HelpFunctions/descriptives.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-
-# compute descriptives (mean and sd) for variables in a data set
-descriptives =function(data){ data.table(sapply(data, function(x)
-  c(
-    mean = mean(x, na.rm = T), sd = sd(x, na.rm = T)
-  )))
-}
